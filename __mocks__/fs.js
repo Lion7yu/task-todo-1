a7f6395e30a58995f9fa98e4afb6cfeb4aa42537
Lionyu <779625444@qq.com>
2021-06-24 19:25:28 +0800

    增加写的测试用例

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -2,24 +2,39 @@
 const fs = jest.genMockFromModule('fs')
 //真正的fs
 const _fs = jest.requireActual('fs') 
-//右边复制到左边
+//右边复制到左边(浅拷贝)
 Object.assign(fs,_fs)
 
-const mocks = {}
+const readMocks = {}
 
-fs.setMock = (path,error,data) =>{
+fs.setReadFileMock = (path,error,data) =>{
   //只要读path，就返回这两个回调的参数
-  mocks[path] = [error,data]
+  readMocks[path] = [error,data]
 }
 
 fs.readFile =(path,options,callback)=>{
   //防御性编程，防止用户不传callback
   if(callback === undefined){callback = options}
   //拦截
-  if(path in mocks){
-    callback(...mocks[path])
+  if(path in readMocks){
+    callback(...readMocks[path])
   }else{
     _fs.readFile(path,options,callback)
+  }
+}
+
+const writeMocks = {}
+
+fs.setWriteFileMock = (path,fn)=>{
+  writeMocks[path] = fn
+}
+
+fs.writeFile = (path,data,options,callback)=>{
+  //区别在于调用真实的fs，还是mock的fs
+  if(path in writeMocks){
+    writeMocks[path](path,data,options,callback)
+  }else{
+    _fs.writeFile(path,data,options,callback)
   }
 }
 
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -5,9 +5,21 @@ jest.mock('fs')
 describe('db',()=>{
   it('can read',async () =>{
     const data = [{title:'hi',done:'true'}]
-    fs.setMock('/xxx',null,JSON.stringify(data))
+    fs.setReadFileMock('/xxx',null,JSON.stringify(data))
     const list = await db.read('/xxx')
     //进行严格意义上的判断
     expect(list).toStrictEqual(data)
+  })
+  it('can write',async()=>{
+    //声明一个假的文件
+    let fakeFile 
+    //写内容写在fakeFile里面
+    fs.setWriteFileMock('/yyy',(path,data,callback)=>{
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title:'喝水',done:true},{title:'吃饭',done:true}]
+    await db.write(list,'/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list)+'\n')
   })
 })
